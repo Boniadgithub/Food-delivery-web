cc120f5d9fdea99b1188b375ddedcbe753a38b63
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-06 05:19:08 -0700

    header page added

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './home.css'
+import Header from '../../components/Header/Header'
 const home = () => {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
