373b34076e64e6691c0b92814d01f57126dd1993
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-06 05:39:43 -0700

    explore menu added to the home page

---
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './ExploreMenu.css'
+import {menu_list} from '../../assets/assets'
+const ExploreMenu = () => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+       <h1>Explore Our Menu</h1>
+        <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expert expertise. Our mission is satisfy your craving and elevate your dining experience, one deliciuse meal at a time </p>
+        <div className="explore-menu-list">
+            {menu_list.map(()=>{
+                return(
+                    <div key={index} className='explore-menu-list-item'>
+                         <img src={item.menu_image} alt={item.name} className=''/>
+                         <p>{item.menu_name }</p>
+                    </div>
+                )
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default ExploreMenu
