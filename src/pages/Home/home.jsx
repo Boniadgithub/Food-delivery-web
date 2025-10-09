ad3f6a3cd403f3c03439cc058b8894f1c4c08a6e
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-09 16:13:02 -0700

    app download added

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -3,6 +3,8 @@ import './home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import App from '../../App'
+import AppDownload from '../../components/AppDownload/AppDownload'
 const home = () => {
 
 const [category, setCategory] = React.useState("All")
@@ -12,6 +14,7 @@ const [category, setCategory] = React.useState("All")
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category ={category} />
+      <AppDownload/>
     </div>
   )
 }
