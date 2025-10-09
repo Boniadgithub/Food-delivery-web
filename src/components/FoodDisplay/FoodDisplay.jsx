df588bdb987d4737ed9ec8f09a080055463c9758
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-09 14:33:32 -0700

    update product filtering

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -11,7 +11,13 @@ const FoodDisplay = ({category}) => {
         <h2>Top dishes near you </h2>
       <div className="food-display-list">
         {food_list.map ((item,index)=>{
-            return< FoodItem key ={item.id||index} id={item.id||index} name={item.name} description={item.description} price={item.price} image={item.image}/>
+            if(category==="All" || category===item.category){
+               return< FoodItem key ={item.id||index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+            }
+
+
+
+            
         })}
       </div>
     </div>
