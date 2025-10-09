c76d8af60df757f87ad7c0dd0b95b0c153f91ab9
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-09 15:04:03 -0700

    Footer added to the component

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -8,16 +8,12 @@ const FoodDisplay = ({category}) => {
     const {food_list}=useContext(StoreContext)
   return (
     <div className='food-display' id='food-display'>
-        <h2>Top dishes {category !== 'All' ? `- ${category}` : 'Near You'}</h2>
+        <h2>Top dishes {category !== 'All' ? `- ${category}` : 'Near You'} </h2>
       <div className="food-display-list">
-        {food_list.map ((item,index)=>{
-            if(category==="All" || category===item.category){
-               return< FoodItem key ={item.id||index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-            }
+        {food_list .filter(item => category === 'All' || item.category === category) .map ((item,index)=>{
 
 
-
-            
+            return< FoodItem key ={item.id||index} id={item.id||index} name={item.name} description={item.description} price={item.price} image={item.image}/>
         })}
       </div>
     </div>
