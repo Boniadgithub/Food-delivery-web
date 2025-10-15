87763ad34ab8ab59834c23428c8ddac228d8b231
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-14 23:21:06 -0700

    some updates in login

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './LoginPopup.css'
-const LoginPopup = () => {
+import  {assets} from '../../assets/assets'
+const LoginPopup = ({setShowLogin}) => {
+
+  const [currState, setCurrState] = React.useState("Login");
   return (
     <div className='login-popup'>
-       
+       <form  className="login-popup-container">
+        <div className="login-popup-title">
+            <h2>{currState}</h2>
+            <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="close"/>
+        </div>
+        <div className="login-popup-input">
+          {currState==="Login"?<></> :  <input type='text 'placeholder='Your name' required />}
+        
+          <input type='email' placeholder='Email address' required />
+          <input type='password' placeholder='Password' required />
+          
+        </div>
+        <button>{currState==="Sign Up"?"Create account":"Login "}</button>
+       </form>
     </div>
   )
 }
