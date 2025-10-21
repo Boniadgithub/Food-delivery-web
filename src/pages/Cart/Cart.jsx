8f0ac3faa2c624ee311157bd042779d73f273319
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-21 01:25:51 -0700

    update in order page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './cart.css'
 import { StoreContext } from '../../context/StoreContext';
+import { useNavigate } from 'react-router-dom';
 const Cart = () => {
 
   const {cartItems,food_list,removeFromCart,getTotalCartAmount} = React.useContext(StoreContext);
+  const navigate = useNavigate();
   return (
     <div className='cart'>
       <div className="cart-items">
@@ -56,7 +58,7 @@ const Cart = () => {
              <b>${getTotalCartAmount()+2}</b>
           </div>
         </div>
-        <button className='cart-checkout'>Checkout</button>
+        <button  onClick={()=>navigate('/order')} className='cart-checkout'>Checkout</button>
       </div>
       <div className="cart-promocode">
          <div>
