965fcf448c275ad8fb0ee89a92154b5c75544468
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-21 03:18:41 -0700

    Delivey info added

--- src/pages/Placeorder/Placeorder.jsx
+++ src/pages/Placeorder/Placeorder.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import './Placeorder.css'
 const Placeorder = () => {
   return (
-    <div>  
+    <form className='place-order'>  
+      <div className='place-order-left'>
+        <p className='title'>Deivery Information</p>
+        <div className='multi-feilds'>
+         <input type="text" placeholder='First name' />
+          <input type="text" placeholder='Last name' />
+        </div>
+        <input type="text" placeholder='Email Address' /> 
+        <input type="text" placeholder='Steet' />
       
-    </div>
+        <div className='multi-feilds'>
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State' />
+        </div>
+        <div className='multi-feilds'>
+          <input type="text" placeholder='Zip Code' />
+          <input type="text" placeholder='Country' />
+        </div>
+        <input type="text" placeholder='Phone Number' />
+        
+      </div>
+      <div className='place-order-right'>
+
+      </div>
+    </form>
   )
 }
 
